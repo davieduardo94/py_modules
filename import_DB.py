d6753f56f669e7f2c86a49cdab9f210a844e951c
Davi Eduardo <davieduardo94@gmail.com>
2021-11-09 16:40:30 -0400

    Implements formatFiles functions

--- import_DB.py
+++ import_DB.py
@@ -48,3 +48,19 @@ def loadFile(caminho, arquivo):
     # print(sql, '\n')
     conectSQL()
     odbcExec(sql)
+    
+def formatFiles(filelist):
+    print('Ajustando arquivos CSV')
+    for file in tqdm(filelist, total=len(filelist)):
+        # print('Carregando: '+file)
+		# abrindo o csv da pasta raiz
+        with open(csvDest+file, 'r', newline='', encoding="utf-8") as csv_file:
+            # realizando ajustes do arquivo
+            csv_file =  ''.join([line for line in csv_file]).replace("	","").replace(",","	").replace("	 "," ").replace('"','')
+            # gerando novo arquivo csv formatado
+            newCSV = open(csvEdit+file[:len(file)-4]+'.csv', 'w', encoding="utf-8", newline='')
+            # escrevendo dados extraidos do csv_file (raiz)
+            newCSV.writelines(csv_file)
+            # fechando o arquivo
+            newCSV.close()
+            time.sleep(0.5)
